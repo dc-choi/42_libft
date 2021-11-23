2f251f9380f746e102fe09e50281a37ddff77c45
Dongchul Choi <donchoi@c5r1s1.42seoul.kr>
2021-11-23 12:27:12 +0900

    norm check

---
+++ libft.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: donchoi <donchoi.student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/23 11:52:45 by donchoi           #+#    #+#             */
+/*   Updated: 2021/11/23 12:10:19 by donchoi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <string.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+int		ft_isalpha(int i);
+int		ft_isdigit(int i);
+int		ft_isalnum(int i);
+int		ft_isascii(int i);
+int		ft_isprint(int i);
+
+#endif
