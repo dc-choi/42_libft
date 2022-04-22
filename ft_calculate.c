1b26e6adf2e703567fbf360bf538433f8004df66
Dongchul Choi <donchoi@c5r1s1.42seoul.kr>
2022-04-22 16:24:20 +0900

    add printf function

---
+++ ft_calculate.c
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calculate.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: donchoi <donchoi.student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/22 16:16:12 by donchoi           #+#    #+#             */
+/*   Updated: 2022/04/22 16:16:31 by donchoi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_calculate(long long nbr, char *base, int baselen)
+{
+	if (nbr == 0)
+		return ;
+	ft_calculate(nbr / baselen, base, baselen);
+	write(1, &base[nbr % baselen], 1);
+}
